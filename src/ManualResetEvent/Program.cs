87e22d32132d9105e5ad58ea8f9562592c29c879
geek <keeper2002@mail.ru>
2018-08-11 06:09:21 -0700

    add ManualResetEvent

---
+++ src/ManualResetEvent/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace ManualResetEvent
+{
+    internal class Program
+    {
+        private static readonly System.Threading.ManualResetEvent manualResetEvent =
+            new System.Threading.ManualResetEvent(true);
+
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("App started");
+            Runner();
+            Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] press Any key to pause");
+            Console.ReadKey();
+            manualResetEvent.Reset();
+            Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] press Any key to continue");
+            Console.ReadKey();
+            manualResetEvent.Set();
+            Console.ReadKey();
+        }
+
+
+        public static Task Runner()
+        {
+            return Task.Run(() =>
+            {
+                while (true)
+                {
+                    manualResetEvent.WaitOne();
+                    Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] {nameof(Runner)} has done operation {DateTime.Now:mm:ss.ms}");
+                    Task.Delay(TimeSpan.FromMilliseconds(500));
+                }
+            });
+        }
+    }
+}
