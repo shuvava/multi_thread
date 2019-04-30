da5bf5466df63b4f62f994f36290a48c94ad358f
Vlad shurygin <Vladimir.Shurygin@worksafebc.com>
2019-04-30 14:45:54 -0700

    parralel execution

---
+++ src/task-parallel-exec/AsyncEx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace task_parallel_exec
+{
+    public static class AsyncEx
+    {
+        public static async Task ParallelForEachAsync<T>(this IEnumerable<T> source, Func<T, Task> asyncAction, int maxDegreeOfParallelism = 10)
+        {
+            var semaphoreSlim = new SemaphoreSlim(maxDegreeOfParallelism);
+            var tcs = new TaskCompletionSource<object>();
+            var exceptions = new ConcurrentBag<Exception>();
+            bool addingCompleted = false;
+
+            foreach (T item in source)
+            {
+                await semaphoreSlim.WaitAsync();
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                asyncAction(item).ContinueWith(t =>
+                 {
+                     semaphoreSlim.Release();
+
+                     if (t.Exception != null)
+                     {
+                         exceptions.Add(t.Exception);
+                     }
+
+                     if (Volatile.Read(ref addingCompleted) && semaphoreSlim.CurrentCount == maxDegreeOfParallelism)
+                     {
+                         tcs.SetResult(null);
+                     }
+                 });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            }
+
+            Volatile.Write(ref addingCompleted, true);
+            await tcs.Task;
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+    }
+}
---
+++ src/task-parallel-exec/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace task_parallel_exec
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Application started");
+
+            var messages = new List<string>();
+            foreach (var i in Enumerable.Range(0, 100)) messages.Add($"hello world {i}");
+
+            var s1 = Stopwatch.StartNew();
+            TplUsage(messages);
+            s1.Stop();
+            var s2 = Stopwatch.StartNew();
+            SemaphoreSlimUsage(messages).Wait();
+            s2.Stop();
+            Console.WriteLine("task submitted");
+            const int m = 10000;
+            Console.WriteLine(((double)(s1.Elapsed.TotalMilliseconds * 1000000) /
+                               m).ToString("0.00 ns"));
+            Console.WriteLine(((double)(s2.Elapsed.TotalMilliseconds * 1000000) /
+                               m).ToString("0.00 ns"));
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+
+        private static void TplUsage(IEnumerable<string> messages, int maxDegree = 10)
+        {
+            Parallel.ForEach(
+                messages,
+                new ParallelOptions {MaxDegreeOfParallelism = maxDegree},
+                msg =>
+                {
+                    Process(msg).Wait();
+                }
+            );
+        }
+
+        private static Task SemaphoreSlimUsage(IEnumerable<string> messages, int maxDegree = 10)
+        {
+            return messages.ParallelForEachAsync(Process);
+        }
+
+
+        public static async Task Process(string msg)
+        {
+            await Task.Delay(100);
+            Print(msg);
+        }
+
+        public static void Print(string msg)
+        {
+            Console.WriteLine($"Thread id {Thread.CurrentThread.ManagedThreadId} : {msg}");
+        }
+    }
+}
